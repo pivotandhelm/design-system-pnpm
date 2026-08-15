94265f36f9cff5face704323dea4705cc6bd211f
Babatunde <pivotandhelm@gmail.com>
2026-08-15 10:31:47 -0500

    format check

--- packages/tokens/src/generated/tokens.ts
+++ packages/tokens/src/generated/tokens.ts
@@ -6,46 +6,46 @@
  */
 
 export const tokens = {
-  "primitive": {
-    "color": {
-      "blue": {
+  primitive: {
+    color: {
+      blue: {
         "10": "#c5dcf2",
         "40": "#8ab8e3",
         "50": "#246aab",
         "60": "#1a4d7c",
         "90": "#1e354a"
       },
-      "gold": {
+      gold: {
         "10": "#f2d5c2",
         "40": "#f3ab7a",
         "50": "#c17a4a",
         "90": "#422a19"
       },
-      "gray": {
+      gray: {
         "0": "#ffffff",
         "10": "#e8e9ed",
         "40": "#cfd0d4",
         "60": "#7c7d80",
         "90": "#2d3142"
       },
-      "teal": {
+      teal: {
         "60": "#0d5f6f"
       },
-      "red": {
+      red: {
         "50": "#cf3830"
       },
-      "green": {
+      green: {
         "50": "#8f9923"
       }
     },
-    "type": {
-      "weight": {
-        "bold": 700,
-        "semibold": 600,
-        "medium": 500,
-        "regular": 400
+    type: {
+      weight: {
+        bold: 700,
+        semibold: 600,
+        medium: 500,
+        regular: 400
       },
-      "size": {
+      size: {
         "100": 9,
         "200": 11,
         "300": 13,
@@ -88,20 +88,20 @@ export const tokens = {
         "1000": -1
       }
     },
-    "layout": {
-      "breakpoints": {
-        "max": 1440,
-        "phone": 360,
-        "desktop": 1024,
-        "tablet": 768,
-        "min": 320,
-        "xs": 360,
-        "lg": 1024,
-        "md": 768,
-        "sm": 480,
-        "xl": 1280
+    layout: {
+      breakpoints: {
+        max: 1440,
+        phone: 360,
+        desktop: 1024,
+        tablet: 768,
+        min: 320,
+        xs: 360,
+        lg: 1024,
+        md: 768,
+        sm: 480,
+        xl: 1280
       },
-      "grids": {
+      grids: {
         "margin-desktop": 64,
         "margin-mobile": 24,
         "margin-tablet": 32,
@@ -113,78 +113,78 @@ export const tokens = {
         "columns-desktop": 12
       }
     },
-    "effect": {
+    effect: {
       "position-x": {
-        "neg4": -4,
-        "neg3": -3,
-        "neg2": -2,
-        "neg1": -1,
-        "none": 0,
-        "pos1": 1,
-        "pos2": 2,
-        "pos3": 3,
-        "pos4": 4
+        neg4: -4,
+        neg3: -3,
+        neg2: -2,
+        neg1: -1,
+        none: 0,
+        pos1: 1,
+        pos2: 2,
+        pos3: 3,
+        pos4: 4
       },
-      "spread": {
-        "pos1": 1,
-        "pos2": 2,
-        "pos3": 3,
-        "pos4": 4,
-        "none": 0
+      spread: {
+        pos1: 1,
+        pos2: 2,
+        pos3: 3,
+        pos4: 4,
+        none: 0
       },
       "glass-frost": {
-        "small": 1,
-        "medium": 2,
-        "large": 4,
-        "none": 0
+        small: 1,
+        medium: 2,
+        large: 4,
+        none: 0
       },
       "position-y": {
-        "neg4": -4,
-        "neg3": -3,
-        "neg2": -2,
-        "neg1": -1,
-        "none": 0,
-        "pos1": 1,
-        "pos2": 2,
-        "pos3": 3,
-        "pos4": 4
+        neg4: -4,
+        neg3: -3,
+        neg2: -2,
+        neg1: -1,
+        none: 0,
+        pos1: 1,
+        pos2: 2,
+        pos3: 3,
+        pos4: 4
       },
-      "blur": {
-        "none": 0,
-        "pos1": 1,
-        "pos2": 2,
-        "pos3": 3,
-        "pos4": 4
+      blur: {
+        none: 0,
+        pos1: 1,
+        pos2: 2,
+        pos3: 3,
+        pos4: 4
       },
       "glass-depth": {
-        "small": 20,
-        "medium": 40,
-        "large": 60,
-        "none": 0
+        small: 20,
+        medium: 40,
+        large: 60,
+        none: 0
       }
     },
-    "text": {
-      "family": {
-        "serif": "Roboto Serif",
-        "mono": "Roboto Mono",
+    text: {
+      family: {
+        serif: "Roboto Serif",
+        mono: "Roboto Mono",
         "san-serif": "Inter"
       },
-      "weight": {
-        "italic": "Italic",
-        "bold": 700,
-        "semibold": 600,
-        "medium": 500,
-        "regular": 400
+      weight: {
+        italic: "Italic",
+        bold: 700,
+        semibold: 600,
+        medium: 500,
+        regular: 400
       },
       "text-decoration": {
-        "none": "none",
-        "underline": "underline"
+        none: "none",
+        underline: "underline"
       },
       "text-transform": {
-        "none": "none",
-        "uppercase": "uppercase"
+        none: "none",
+        uppercase: "uppercase"
       },
-      "size": {
+      size: {
         "100": 9,
         "200": 11,
         "300": 13,
@@ -227,14 +227,14 @@ export const tokens = {
         "1000": -1
       },
       "paragraph-spacing": {
-        "none": 0,
-        "sm": 8,
-        "md": 16,
-        "lg": 24,
-        "xl": 32
+        none: 0,
+        sm: 8,
+        md: 16,
+        lg: 24,
+        xl: 32
       }
     },
-    "space": {
+    space: {
       "4": 4,
       "8": 8,
       "12": 12,
@@ -246,7 +246,7 @@ export const tokens = {
       "96": 96,
       "128": 128,
       "160": 160,
-      "none": 0,
+      none: 0,
       "neg-4": -4,
       "neg-12": -12,
       "neg-16": -16,
@@ -254,19 +254,19 @@ export const tokens = {
       "neg-8": -8,
       "neg-24": -24
     },
-    "border": {
-      "radius": {
-        "full": 999,
-        "md": 8,
-        "sm": 4,
-        "xs": 2,
-        "xl": 16,
-        "none": 0,
-        "lg": 12,
+    border: {
+      radius: {
+        full: 999,
+        md: 8,
+        sm: 4,
+        xs: 2,
+        xl: 16,
+        none: 0,
+        lg: 12,
         "3xl": 32,
         "2xl": 24
       },
-      "weight": {
+      weight: {
         "1": 1,
         "2": 2,
         "4": 4,
@@ -276,25 +276,25 @@ export const tokens = {
       }
     },
     "z-index": {
-      "base": 0,
-      "raised": 100,
-      "sticky": 200,
-      "floating": 400,
-      "overlay": 800,
-      "modal": 1600
+      base: 0,
+      raised: 100,
+      sticky: 200,
+      floating: 400,
+      overlay: 800,
+      modal: 1600
     }
   },
-  "semantic": {
-    "color": {
-      "background": {
-        "primary": "#1a4d7c",
-        "accent": "#c17a4a",
+  semantic: {
+    color: {
+      background: {
+        primary: "#1a4d7c",
+        accent: "#c17a4a",
         "gray-lightest": "#e8e9ed",
         "gray-dark": "#2d3142",
-        "teal": "#0d5f6f",
-        "white": "#ffffff",
-        "red": "#cf3830",
-        "green": "#8f9923",
+        teal: "#0d5f6f",
+        white: "#ffffff",
+        red: "#cf3830",
+        green: "#8f9923",
         "accent-lightest": "#f2d5c2",
         "primary-light": "#246aab",
         "primary-lightest": "#c5dcf2",
@@ -303,147 +303,147 @@ export const tokens = {
         "primary-button": "#246aab",
         "primary-lighter": "#8ab8e3"
       },
-      "text": {
-        "primary": "#1a4d7c",
-        "accent": "#c17a4a",
+      text: {
+        primary: "#1a4d7c",
+        accent: "#c17a4a",
         "lightest-gray": "#e8e9ed",
-        "dark": "#2d3142",
-        "white": "#ffffff",
+        dark: "#2d3142",
+        white: "#ffffff",
         "primary-light": "#8ab8e3",
         "accent-lightest": "#f2d5c2",
-        "teal": "#0d5f6f",
-        "red": "#cf3830",
-        "green": "#8f9923",
+        teal: "#0d5f6f",
+        red: "#cf3830",
+        green: "#8f9923",
         "primary-link": "#246aab",
         "accent-lighter": "#f3ab7a",
         "light-gray": "#cfd0d4",
         "gray-mid": "#7c7d80"
       },
-      "stroke": {
-        "primary": "#1a4d7c",
-        "accent": "#c17a4a",
+      stroke: {
+        primary: "#1a4d7c",
+        accent: "#c17a4a",
         "gray-lightest": "#e8e9ed",
-        "teal": "#0d5f6f",
-        "white": "#ffffff",
+        teal: "#0d5f6f",
+        white: "#ffffff",
         "primary-lightest": "#c5dcf2",
         "accent-lightest": "#f2d5c2",
         "gray-light": "#cfd0d4",
-        "red": "#cf3830",
-        "green": "#8f9923",
+        red: "#cf3830",
+        green: "#8f9923",
         "primary-link": "#246aab",
-        "dark": "#2d3142",
+        dark: "#2d3142",
         "accent-light": "#f3ab7a",
         "gray-mid": "#7c7d80"
       },
-      "effect": {
-        "dropshadow": "#1a4a7c40",
-        "overlay": "#1a4a7c80",
-        "blanket": "#1a4a7cbf",
-        "glass": "#c5dcf259"
+      effect: {
+        dropshadow: "#1a4a7c40",
+        overlay: "#1a4a7c80",
+        blanket: "#1a4a7cbf",
+        glass: "#c5dcf259"
       }
     },
-    "text": {
+    text: {
       "body-xxs": {
-        "family": "Roboto Serif",
+        family: "Roboto Serif",
         "line-height": 14,
-        "size": 9,
+        size: 9,
         "letter-spacing": 0.25,
-        "weight": 400,
+        weight: 400,
         "paragraph-spacing": 16
       },
       "body-xs": {
-        "family": "Inter",
+        family: "Inter",
         "letter-spacing": 0,
         "line-height": 16,
-        "size": 11,
-        "weight": 400,
+        size: 11,
+        weight: 400,
         "paragraph-spacing": 16
       },
       "label-s": {
-        "family": "Inter",
+        family: "Inter",
         "letter-spacing": 0.25,
         "line-height": 20,
-        "size": 13.5,
-        "weight": 600
+        size: 13.5,
+        weight: 600
       },
       "label-m": {
-        "family": "Inter",
+        family: "Inter",
         "line-height": 24,
-        "size": 15,
-        "weight": 600,
+        size: 15,
+        weight: 600,
         "letter-spacing": 0.5
       },
       "body-s": {
-        "family": "Inter",
+        family: "Inter",
         "line-height": 20,
-        "size": 13,
+        size: 13,
         "letter-spacing": 0,
-        "weight": 400,
+        weight: 400,
         "paragraph-spacing": 24
       },
       "body-m": {
-        "family": "Inter",
+        family: "Inter",
         "letter-spacing": 0,
         "line-height": 24,
-        "size": 16,
-        "weight": 400,
+        size: 16,
+        weight: 400,
         "paragraph-spacing": 24
       },
       "body-l": {
-        "family": "Inter",
+        family: "Inter",
         "letter-spacing": 0,
-        "size": 19,
+        size: 19,
         "line-height": 26,
-        "weight": 400,
+        weight: 400,
         "paragraph-spacing": 24
       },
       "heading-s": {
-        "family": "Roboto Serif",
-        "size": 23,
+        family: "Roboto Serif",
+        size: 23,
         "letter-spacing": 0,
         "line-height": 30,
-        "weight": 500
+        weight: 500
       },
       "heading-m": {
-        "family": "Roboto Serif",
+        family: "Roboto Serif",
         "letter-spacing": -0.25,
         "line-height": 33,
-        "size": 28,
-        "weight": 500
+        size: 28,
+        weight: 500
       },
       "heading-l": {
-        "family": "Roboto Serif",
+        family: "Roboto Serif",
         "line-height": 40,
-        "size": 33,
-        "weight": 600,
+        size: 33,
+        weight: 600,
         "letter-spacing": -0.5
       },
       "title-m": {
-        "family": "Roboto Serif",
+        family: "Roboto Serif",
         "letter-spacing": -0.75,
-        "size": 40,
+        size: 40,
         "line-height": 48,
-        "weight": 600
+        weight: 600
       },
       "title-l": {
-        "family": "Roboto Serif",
-        "size": 48,
+        family: "Roboto Serif",
+        size: 48,
         "line-height": 56,
-        "weight": 600,
+        weight: 600,
         "letter-spacing": -1
       },
       "body-xl": {
-        "family": "Inter",
-        "weight": 400,
-        "size": 23,
+        family: "Inter",
+        weight: 400,
+        size: 23,
         "line-height": 30,
         "letter-spacing": 0,
         "paragraph-spacing": 32
       },
       "label-l": {
-        "family": "Inter",
-        "weight": 600,
-        "size": 18,
+        family: "Inter",
+        weight: 600,
+        size: 18,
         "line-height": 26,
         "letter-spacing": 0.75
       }
